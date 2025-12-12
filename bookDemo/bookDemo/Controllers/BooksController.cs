4f47f751422024b89ed4c553ba9218daa1f5b71a
canersinan00 <canersinan1013@gmail.com>
2025-12-12 16:11:47 +0300

    First Commit "Models and Controller Created"

---
+++ bookDemo/bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            var book = ApplicationContext
+                .Books
+                .Where(x => x.Id == id)
+                .SingleOrDefault();
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+
+    }
+}
---
+++ bookDemo/bookDemo/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book() {Id = 1, Title="Karagöz and Hacivat",Price=75},
+                new Book() {Id = 2, Title="Nutuk", Price=150},
+                new Book() {Id = 3, Title="Karagöz and Hacivat",Price=100}
+            };
+        }
+    }
+}
